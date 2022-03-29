ed45cbe222893ede999514072b20205ee67ea6d9
Myo Myint Aung <myomyintaung064@gmail.com>
2022-03-29 10:49:31 +0630

    working on navbar

--- src/components/navbar/Navbar.js
+++ src/components/navbar/Navbar.js
@@ -1,8 +1,20 @@
+import logo from "./../../assets/GPT-3.png";
 import React from "react";
+import { RiMenu3Line, RiCloseLin } from "react-icons";
 import classes from "./Navbar.module.css";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  return (
+    <div className="gpt3__navbar">
+      {/* Navlinks container */}
+      <div className="gpt3__navbar-links">
+        {/* img logo container */}
+        <div className="gpt3__navbar-links_logo">
+          <img src={logo} alt="logo" />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;

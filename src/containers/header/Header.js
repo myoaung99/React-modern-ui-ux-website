4cb09405ec597792679a096c62ec05d37f5b85fc
MyoMyintAung <myomyintaung064@gmail.com>
2022-03-30 23:50:54 +0630

    added HTML header seciton contents

--- src/containers/header/Header.js
+++ src/containers/header/Header.js
@@ -1,8 +1,39 @@
 import React from "react";
-import classes from "./Header.module.css";
+import "./Header.css";
+import people from "../../assets/people.png";
+import ai from "../../assets/ai.png";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <section className="gpt3__header section__padding">
+      {/* Header Content */}
+      <div className="gpt3__header-content">
+        <h1 className="gradient__text">
+          Let's Build Something Amazing With GPT3
+        </h1>
+        <p>
+          Yet bed any for travelling assistance indulgence unpleasing. Not
+          thoughts all exercise blessing. Indulgence way everything joy
+          alteration boisterous the attachment. Party we years to order allow
+          asked of.
+        </p>
+
+        <div className="gpt3__header-container__input">
+          <input type="text" placeholder="Enter your Email" />
+          <button type="button">Get Started</button>
+        </div>
+
+        <div className="gpt3__header-content__people">
+          <img src={people} alt="people" />
+          <p>1,600 people requested access a visit in last 24 hours</p>
+        </div>
+
+        <div className="gpt3__header-content__image">
+          <img src={ai} alt="ai" />
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export default Header;
